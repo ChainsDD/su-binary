49fc519553bebd62bd37290e27cec5553f515159
Adam Shanks <chainsdd@gmail.com>
2011-02-28 23:30:42 +0000

    Initial commit with version 2.3.1

---
+++ Android.mk
@@ -0,0 +1,13 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := su
+LOCAL_SRC_FILES := su.c activity.cpp
+
+
+LOCAL_C_INCLUDES += external/sqlite/dist
+LOCAL_SHARED_LIBRARIES := liblog libsqlite libandroid_runtime
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
+
+include $(BUILD_EXECUTABLE)
