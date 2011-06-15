9b15727135225aaca8699f9caef4222f70637403
Adam Shanks <chainsdd@gmail.com>
2011-06-15 11:33:40 +0100

    Fix Android.mk

--- Android.mk
+++ Android.mk
@@ -6,8 +6,13 @@ LOCAL_SRC_FILES := su.c activity.cpp
 
 
 LOCAL_C_INCLUDES += external/sqlite/dist
-LOCAL_SHARED_LIBRARIES := liblog libsqlite libandroid_runtime
-
+LOCAL_SHARED_LIBRARIES := \
+    liblog \
+    libsqlite \
+    libcutils \
+    libbinder \
+    libutils
 LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
+LOCAL_MODULE_TAGS := eng,debug
 
 include $(BUILD_EXECUTABLE)
