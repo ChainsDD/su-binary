d86184a90e78016fd66f558cd12d54a979b7fb46
Adam Shanks <chainsdd@gmail.com>
2012-06-04 20:23:36 +0100

    Build su as static.
    With so many different versions of Android, using a dynamically linked
    binary is just not practical, since things can be wildly different on
    different versions.

--- Android.mk
+++ Android.mk
@@ -2,21 +2,17 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := su
-LOCAL_SRC_FILES := su.c db.c activity.cpp
-
-SU_SHARED_LIBRARIES := liblog libsqlite
-ifeq ($(PLATFORM_SDK_VERSION),4)
-	LOCAL_CFLAGS += -DSU_LEGACY_BUILD
-	SU_SHARED_LIBRARIES += libandroid_runtime
-else
-	SU_SHARED_LIBRARIES += libcutils libbinder libutils
-	LOCAL_MODULE_TAGS := debug,eng
-endif
+LOCAL_SRC_FILES := su.c db.c activity.c
 
 LOCAL_C_INCLUDES += external/sqlite/dist
 
-LOCAL_SHARED_LIBRARIES := $(SU_SHARED_LIBRARIES)
+LOCAL_STATIC_LIBRARIES := \
+    liblog \
+    libsqlite \
+    libc \
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
+LOCAL_MODULE_TAGS := eng debug
+LOCAL_FORCE_STATIC_EXECUTABLE := true
 
 include $(BUILD_EXECUTABLE)
